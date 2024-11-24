eec01a0489199f11d5ab0e0c47493322a900e754
Felics666 <felics6@gmail.com>
2024-11-25 01:22:11 +0300

    Completed3

--- src/test/java/pro/sky/skyproEmployeeBookTestSpringMockito/service/impl/DepartmentServiceImplTest.java
+++ src/test/java/pro/sky/skyproEmployeeBookTestSpringMockito/service/impl/DepartmentServiceImplTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.skyproEmployeeBookTestSpringMockito.model.Employee;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -26,46 +25,56 @@ class DepartmentServiceImplTest {
     @Mock
     private EmployeeServiceImpl employeeService;
 
-    private final Employee EMP = new Employee("Ivan", "Ivanov", 2000,1);
+    private final Employee EMP = new Employee("Ivan", "Ivanov", 2000, 1);
     private final Employee EMP1 = new Employee("Petr", "Petrov", 2500, 1);
-    private final Employee EMP2 = new Employee("Harry", "Potter",3000,2);
-    private final Employee EMP3 = new Employee("Olga", "Simonova",3500,2);
-    private final Employee EMP4 = new Employee("Anastasia", "Zvyagina",5000,3);
-    private final Employee EMP5 = new Employee("Karina","Smirnova", 5500,3);
+    private final Employee EMP2 = new Employee("Harry", "Potter", 3000, 2);
+    private final Employee EMP3 = new Employee("Olga", "Simonova", 3500, 2);
+    private final Employee EMP4 = new Employee("Anastasia", "Zvyagina", 5000, 3);
+    private final Employee EMP5 = new Employee("Karina", "Smirnova", 5500, 3);
 
     @BeforeEach
     void setUp() {
-        when (employeeService.getAll()).thenReturn(Arrays.asList(EMP, EMP1, EMP2, EMP3, EMP4, EMP5));
+
+        when(employeeService.getAll()).thenReturn(Arrays.asList(EMP, EMP1, EMP2, EMP3, EMP4, EMP5));
+
     }
 
     @Test
     void sumSalary() {
-        assertEquals(6500,departmentService.sumSalary(2));
+
+        assertEquals(6500, departmentService.sumSalary(2));
+
     }
 
     @Test
     void minSalary() {
+
         assertEquals(2000, departmentService.minSalary(1));
+
     }
 
     @Test
     void maxSalary() {
+
         assertEquals(5500, departmentService.maxSalary(3));
+
     }
 
     @Test
     void findByDepartment() {
         List<Employee> employees = departmentService.findByDepartment(2);
-        assertEquals(2,employees.size());
+
+        assertEquals(2, employees.size());
         assertTrue(employees.containsAll(Arrays.asList(EMP2, EMP3)));
     }
 
-
     @Test
     void findAllByDep() {
-        Map<Integer,List<Employee>> allByDep = departmentService.findAllByDep();
+        Map<Integer, List<Employee>> allByDep = departmentService.findAllByDep();
+
         assertEquals(3, allByDep.size());
         assertEquals(2, allByDep.get(2).size());
+
         assertTrue(allByDep.get(3).containsAll(Arrays.asList(EMP4, EMP5)));
 
     }
